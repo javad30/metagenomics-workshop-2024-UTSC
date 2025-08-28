ff29e327f9c7ef2f8b2e98e8214312aa45909859
Javad <Javad.sadeghi1@gmail.com>
2025-08-28 09:29:28 -0400

    Add minimal Sphinx conf.py with RTD mocks

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
 
+# -- Project information -----------------------------------------------------
+project = "Metagenomics Workshop 2024 UTSC"
+author = "Javad Sadeghi"
+release = "0.1"
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    "sphinx.ext.autodoc",    # Automatically document docstrings
+    "sphinx.ext.napoleon",   # Support Google/NumPy style docstrings
+    "sphinx.ext.viewcode",   # Add links to source code
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = "sphinx_rtd_theme"
+
+# -- Workaround for heavy imports on RTD -------------------------------------
+# These packages are mocked so autodoc won't try to import them on RTD
+autodoc_mock_imports = [
+    "numpy",
+    "pandas",
+    "scikit-bio",
+    "biom-format",
+]
